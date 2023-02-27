a7bf5b87db98eb763a83bbd3b076f86da1bc8031
Glenda <gcheung34@my.bcit.ca>
2023-02-27 10:17:25 -0800

    fixed spelling on import ad

---
+++ .history/README_20230227101436.md
@@ -0,0 +1,12 @@
+## Description
+How to create a pop up through React. 
+
+## Tools/Programming Language Used
+- HTML
+- CSS
+- Javascript
+- useState
+
+## Command Lines
+- create-next-app
+- npm run dev
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
-import Ad from '@/components/ad'
+import Ad from '@/components/Ad'
 
 const inter = Inter({ subsets: ['latin'] })
 
